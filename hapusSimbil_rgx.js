4273f4139aceddee2c62446b3f6865acbc148a29
anisfikriyyah <anisfikri3@gmial.com>
2018-07-30 15:03:39 +0700

    sudah di edit

--- hapusSimbil_rgx.js
+++ hapusSimbil_rgx.js
@@ -1,6 +1,6 @@
 function hapusSimbol(str) {
   // you can only write your code here!
-  var hasil = str.match(/[a-z,0-9]+/gi)
+  var hasil = str.match(/[a-z 0-9]+/gi)
   var result = hasil.join('')
   return result
 }
@@ -8,6 +8,6 @@ function hapusSimbol(str) {
 // TEST CASES
 console.log(hapusSimbol('test%$4aa')); // test4aa
 console.log(hapusSimbol('devel0p3r s3j@@ati')); // devel0p3rs3jati
-console.log(hapusSimbol('ma@#k!an~')); // makan
+console.log(hapusSimbol('ma@#k,!an~')); // makan
 console.log(hapusSimbol('coding')); // coding
 console.log(hapusSimbol('1+3-5*2=100')); // 1352100
